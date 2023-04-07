7c09c367e47e34baa391c3a80f7e7ee7bf2e0ed8
WindyWisper <umalib629@outlook.com>
2023-04-07 15:54:33 +0800

    feat: init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./renderer/app.vue";
+import router from "./renderer/router";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+createApp(App).use(ElementPlus).use(router).mount("#app");
