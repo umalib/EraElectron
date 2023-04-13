12c01ac1c8946b7e227c7886ea5d7993572fdc6e
WindyWisper <umalib629@outlook.com>
2023-04-13 12:14:26 +0800

    docs: add docs in era-electron lib

--- example/ERE/era-electron.js
+++ example/ERE/era-electron.js
@@ -1,13 +1,13 @@
 module.exports = {
   /**
    * add a character
-   * @param charaId {integer} REQUIRED id of the character defined in chara*.csv
+   * @param {integer} charaId REQUIRED id of the character defined in chara*.csv
    * @returns {void}
    */
   addCharacter(charaId) {},
   /**
    * clear printed lines
-   * @param lineCount {integer} how many lines to be cleared, leave undefined to clear all
+   * @param {integer} [lineCount] how many lines to be cleared, leave undefined to clear all
    * @returns {void}
    */
   clear(lineCount) {},
@@ -18,7 +18,7 @@ module.exports = {
   drawLine() {},
   /**
    * get a variable by name
-   * @param varName {string} REQUIRED the name of variable, String, like 'callname:1:2'
+   * @param {string} varName REQUIRED the name of variable, String, like 'callname:1:2'
    * @returns {any|undefined} the variable, or undefined if failed
    */
   get(varName) {},
@@ -34,13 +34,13 @@ module.exports = {
   getAllCharacters() {},
   /**
    * wait for use's input
-   * @param config {{rule: string}} the config of input, like rule
+   * @param {{rule: string}} [config] the config of input, like rule
    * @returns {Promise<any>} a promise of the user input, please use await
    */
   async input(config) {},
   /**
    * load a save file
-   * @param savIndex {integer} REQUIRED the index of the save file
+   * @param {integer} savIndex REQUIRED the index of the save file
    * @returns {boolean} if the loading succeeds
    */
   load(savIndex) {},
@@ -51,16 +51,16 @@ module.exports = {
   loadGlobal() {},
   /**
    * print some text
-   * @param content {string} REQUIRED the text to be printed
-   * @param config {{align: string, isParagraph: boolean, offset: integer, p: boolean, width: integer}} the config of the printed line
+   * @param {string} content REQUIRED the text to be printed
+   * @param {{align: string, isParagraph: boolean, offset: integer, p: boolean, width: integer}} [config] the config of the printed line
    * @returns {void}
    */
   print(content, config) {},
   /**
    * print a button
-   * @param content {string} REQUIRED the text of the button
-   * @param accelerator {integer} REQUIRED the accelerator key of the button used in the input
-   * @param config {{isButton: boolean, type: string, offset: integer, width: integer, align: string}} the config of the printed button
+   * @param {string} content REQUIRED the text of the button
+   * @param {integer} accelerator REQUIRED the accelerator key of the button used in the input
+   * @param {{isButton: boolean, type: string, offset: integer, width: integer, align: string}} [config] the config of the printed button
    * @returns {void}
    */
   printButton(content, accelerator, config) {},
@@ -71,10 +71,10 @@ module.exports = {
   println() {},
   /**
    * print a progress bar
-   * @param percentage {float} REQUIRED the percentage of the progress bar, 0-100
-   * @param inContent {string} REQUIRED the text shown in the progress bar
-   * @param outContent {string} REQUIRED the text shown next to the progress bar
-   * @param config {{align: string, barRatio: float, color: string, fontColor: string, height: integer, offset: integer, width: integer}} the config of the printed progress
+   * @param {float} percentage REQUIRED the percentage of the progress bar, 0-100
+   * @param {string} inContent REQUIRED the text shown in the progress bar
+   * @param {string} outContent REQUIRED the text shown next to the progress bar
+   * @param {{align: string, barRatio: float, color: string, fontColor: string, height: integer, offset: integer, width: integer}} [config] the config of the printed progress
    * @returns {void}
    */
   printProgress(percentage, inContent, outContent, config) {},
@@ -85,7 +85,7 @@ module.exports = {
   resetAllExceptGlobal() {},
   /**
    * reset the data of a character
-   * @param charaId {integer} REQUIRED id of the character defined in chara*.csv
+   * @param {integer} charaId REQUIRED id of the character defined in chara*.csv
    * @returns {void}
    */
   resetCharacter(charaId) {},
@@ -96,7 +96,7 @@ module.exports = {
   resetGlobal() {},
   /**
    * save data into a save file
-   * @param savIndex {integer} REQUIRED the index of the save file
+   * @param {integer} savIndex REQUIRED the index of the save file
    * @returns {boolean} if the saving succeeds
    */
   save(savIndex) {},
@@ -107,32 +107,32 @@ module.exports = {
   saveGlobal() {},
   /**
    * set a variable by name
-   * @param varName {string} REQUIRED the name of variable, String, like 'callname:1:2'
-   * @param val {any} the new value of the variable
+   * @param {string} varName REQUIRED the name of variable, String, like 'callname:1:2'
+   * @param {any} val REQUIRED the new value of the variable
    * @returns {any|undefined} the new value of the variable, or undefined if failed
    */
   set(varName, val) {},
   /**
    * set default text-align
-   * @param textAlign {string} the text-align, center, left or right used in usual
+   * @param {string} textAlign REQUIRED the text-align, center, left or right used in usual
    * @returns {void}
    */
   setAlign(textAlign) {},
   /**
    * set default offset
-   * @param offset {integer} an integer between 0 and 23
+   * @param {integer} offset REQUIRED an integer between 0 and 23
    * @returns {void}
    */
   setOffset(offset) {},
   /**
    * set the title of the window
-   * @param title {string}
+   * @param {string} title REQUIRED
    * @returns {void}
    */
   setTitle(title) {},
   /**
    * set default width
-   * @param width {integer} an integer between 1 and 24
+   * @param {integer} width REQUIRED an integer between 1 and 24
    * @returns {void}
    */
   setWidth(width) {},
