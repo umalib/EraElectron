1c7bdb1fc926c70b456de21ad06655638ac29872
WindyWisper <umalib629@outlook.com>
2023-04-16 14:43:37 +0800

    chore: move example game to new repo

--- game/ere/system/sys_call_recruit.js
+++
@@ -1,8 +0,0 @@
-﻿module.exports = (ind_target, extra_flag) => {
-  const { existsSync } = require('fs');
-  if (existsSync(`../event/recruit/rec_${ind_target}.js`)) {
-    require(`../event/recruit/rec_${ind_target}`)(extra_flag);
-  } else {
-    require('../event/recruit/rec_common')(ind_target, extra_flag);
-  }
-};
